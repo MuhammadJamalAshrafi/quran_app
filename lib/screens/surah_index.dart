fc93ae44cefb7face1d9f1b37ca77176fb80bd94
Muhammad Jamal Ashrafi <33508875+MuhammadJamalAshrafi@users.noreply.github.com>
2022-04-05 09:15:58 +0500

    Surah Index Page

--- lib/screens/surah_index.dart
+++ lib/screens/surah_index.dart
@@ -7,7 +7,7 @@ class SurahIndex extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Quran App"),
+        title: const Text("Quran App"),
       ),
       backgroundColor: Colors.white,
       body: Column(
@@ -15,8 +15,12 @@ class SurahIndex extends StatelessWidget {
           Expanded(
               child: ListView.builder(
                 itemCount: 10,
-                  itemBuilder: (_,index) => ListTile(
-                    title: Text("test"),
+                  itemBuilder: (_,index) => const Card(
+                    child: ListTile(
+                      leading: Text("1."),
+                      title: Text("Al-Fatiha", style: TextStyle(fontSize: 16, color: Colors.black)),
+                      subtitle: Text("The Opener", style: TextStyle(fontSize: 12, color: Color(0xFF888888))),
+                    ),
                   ),
               ),
           ),
