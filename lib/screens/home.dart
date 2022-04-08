1925edcd02f4d9081665e7f9c47819b04e0ed02c
Muhammad Jamal Ashrafi <33508875+MuhammadJamalAshrafi@users.noreply.github.com>
2022-04-08 09:53:38 +0500

    home page design

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,7 +8,15 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        leading: const ImageIcon(AssetImage("images/menu.png"), color: Colors.black),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              iconSize: 50,
+              icon: const ImageIcon(AssetImage("images/menu.png"), color: Colors.black),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+            );
+          }
+        ),
         leadingWidth: 100,
         title: RichText(
           text: const TextSpan(
@@ -22,65 +30,166 @@ class Home extends StatelessWidget {
             ]
           ),
         ),
-        // title: Text("Quran App", style: TextStyle(color: Colors.black, fontSize: 36, fontWeight: FontWeight.w700)),
         elevation: 0.0,
         centerTitle: true,
       ),
+        drawer: Drawer(),
         backgroundColor: Colors.white,
         body: Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20),
+          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 60),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              const SizedBox(height: 43),
+              const Text("As Salam u Alaikum",style: TextStyle(color: Colors.black, fontFamily: "Poppins", fontSize: 24, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 15),
               Container(
-                height: 110,
-                width: MediaQuery.of(context).size.width,
-                child: const Center(
-                  child: Text("1. Surah Index",
-                    style:TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white
-                    )),
-                ),
                 decoration: BoxDecoration(
-                    color: const Color(0xFF54A7BC),
-                    borderRadius: BorderRadius.circular(10)
+                  border: Border.all(color: const Color(0xFFE5E5E5), width: 1)
                 ),
               ),
-              SizedBox(height: 18),
+              const SizedBox(height: 19),
               Container(
                 height: 110,
                 width: MediaQuery.of(context).size.width,
-                child: const Center(
-                  child: Text("2. Juz Index",
-                      style:TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white
-                      )),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    children:[
+                      const Text("1. Surah Index",
+                          style:TextStyle(
+                              fontSize: 20,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white
+                          )),
+                      const Spacer(),
+                      Container(
+                        width: 70,
+                        height: 70,
+                        child: Container(
+                          width: 7.56,
+                          height: 16,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("images/arrow.png"),
+                            )
+                          ),
+                        ),
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("images/polygon.png")
+                          )
+                        ),
+                      )
+                    ],
+                  ),
                 ),
                 decoration: BoxDecoration(
-                    color: const Color(0xFF54A7BC),
-                    borderRadius: BorderRadius.circular(10)
+                    borderRadius: BorderRadius.circular(10),
+                  image: const DecorationImage(
+                    image: AssetImage("images/div_bg.png"),
+                    fit: BoxFit.cover
+                  )
                 ),
               ),
-              SizedBox(height: 18),
+              const SizedBox(height: 18),
               Container(
                 height: 110,
                 width: MediaQuery.of(context).size.width,
-                child: const Center(
-                  child: Text("3. Sajda Index",
-                      style:TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white
-                      )),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    children:[
+                      const Text("2. Juz Index",
+                          style:TextStyle(
+                              fontSize: 20,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white
+                          )),
+                      const Spacer(),
+                      Container(
+                        width: 70,
+                        height: 70,
+                        child: Container(
+                          width: 7.56,
+                          height: 16,
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("images/arrow.png"),
+                              )
+                          ),
+                        ),
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("images/polygon.png")
+                            )
+                        ),
+                      )
+                    ],
+                  ),
                 ),
                 decoration: BoxDecoration(
-                    color: const Color(0xFF54A7BC),
-                    borderRadius: BorderRadius.circular(10)
+                    borderRadius: BorderRadius.circular(10),
+                    image: const DecorationImage(
+                        image: AssetImage("images/div_bg.png"),
+                        fit: BoxFit.cover
+                    )
+                ),
+              ),
+              const SizedBox(height: 18),
+              Container(
+                height: 110,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    children:[
+                      const Text("3. Sajda Index",
+                          style:TextStyle(
+                              fontSize: 20,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white
+                          )),
+                      const Spacer(),
+                      Container(
+                        width: 70,
+                        height: 70,
+                        child: Container(
+                          width: 7.56,
+                          height: 16,
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("images/arrow.png"),
+                              )
+                          ),
+                        ),
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("images/polygon.png")
+                            )
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: const DecorationImage(
+                        image: AssetImage("images/div_bg.png"),
+                        fit: BoxFit.cover
+                    )
+                ),
+              ),
+              Spacer(),
+              Container(
+                width: 180,
+                height: 180,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("images/40%_opacity_bg.png")
+                  )
                 ),
               )
             ],
